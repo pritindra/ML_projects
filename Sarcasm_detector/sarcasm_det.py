2bfddb1625c9a22157416645eeb05042044b5bd2
Pritindra <pritindra.d@gmail.com>
2021-07-06 14:50:38 +0530

    tokenization

--- Sarcasm_detector/sarcasm_det.py
+++ Sarcasm_detector/sarcasm_det.py
@@ -1,3 +1,38 @@
-import tensorflow as tf
+# import tensorflow as tf
 import numpy as np
+import pandas as pd
+import regex as re
+import nltk
+import itertools
+
+sar_acc = pd.read_json('Sarcasm_Headlines_Dataset.json',lines=True)
+sar_acc['source'] = sar_acc['article_link'].apply(lambda x: re.findall(r'\w+', x)[2])
+
+# Tokenization
+sar_det = sar_acc[sar_acc.is_sarcastic==1]
+sar_det.reset_index(drop=True, inplace=True)
+acc_det = sar_acc[sar_acc.is_sarcastic==0]
+acc_det.reset_index(drop=True, inplace=True)
+
+sar_news = []
+for rows in range(0, sar_det.shape[0]):
+    head_txt = sar_det.headline[rows]
+    head_txt = head_txt.split(" ")
+    sar_news.append(head_txt)
+
+sar_list = list(itertools.chain(*sar_news))
+
+acc_news = []
+for rows in range(0, acc_det.shape[0]):
+    head_txt = acc_det.headline[rows]
+    head_txt = head_txt.split(" ")
+    acc_news.append(head_txt)
+
+acc_list = list(itertools.chain(*sar_news))
+
+stopwords = nltk.corpus.stopwords.words('english')
+sar_list_restp = [word for word in sar_list if word.lower() not in stopwords]
+acc_list_restp = [word for word in acc_list if word.lower() not in stopwords]
+
+
 
