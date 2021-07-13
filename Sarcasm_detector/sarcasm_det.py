47da03ae3128d0f2268b307e5b4e8a64099f6614
Pritindra <pritindra.d@gmail.com>
2021-07-13 09:08:55 +0530

    changes minor

--- Sarcasm_detector/sarcasm_det.py
+++ Sarcasm_detector/sarcasm_det.py
@@ -3,36 +3,42 @@ import numpy as np
 import pandas as pd
 import regex as re
 import nltk
-import itertools
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from sklearn.metrics import accuracy_score
+
 
 sar_acc = pd.read_json('Sarcasm_Headlines_Dataset.json',lines=True)
 sar_acc['source'] = sar_acc['article_link'].apply(lambda x: re.findall(r'\w+', x)[2])
 
-# Tokenization
-sar_det = sar_acc[sar_acc.is_sarcastic==1]
-sar_det.reset_index(drop=True, inplace=True)
-acc_det = sar_acc[sar_acc.is_sarcastic==0]
-acc_det.reset_index(drop=True, inplace=True)
 
-sar_news = []
-for rows in range(0, sar_det.shape[0]):
-    head_txt = sar_det.headline[rows]
-    head_txt = head_txt.split(" ")
-    sar_news.append(head_txt)
+X = sar_acc.headline
+Y = sar_acc.is_sarcastic
+le = LabelEncoder()
+Y = le.fit_transform(Y)
+Y = Y.reshape(-1,1)
 
-sar_list = list(itertools.chain(*sar_news))
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2)
+tk = tf.keras.preprocessing.text.Tokenizer(num_words=1000)
+tk.fit_on_texts(X_train)
+seqs = tk.texts_to_sequences(X_train)
+max_len = 100
+seqs_mat = tf.keras.preprocessing.sequence.pad_sequences(seqs,maxlen=max_len)
 
-acc_news = []
-for rows in range(0, acc_det.shape[0]):
-    head_txt = acc_det.headline[rows]
-    head_txt = head_txt.split(" ")
-    acc_news.append(head_txt)
+def model_def():
+    inputs = tf.keras.layers.Input(name='inputs', shape=[max_len])
+    layer = tf.keras.layers.Embedding(1000,50,input_length=max_len)(inputs)
+    layer = tf.keras.layers.LSTM(64)(layer)
+    layer = tf.keras.layers.Dense(256,name='FC1')(layer)
+    layer = tf.keras.layers.Activation('relu')(layer)
+    layer = tf.keras.layers.Dropout(0.2)(layer)
+    layer = tf.keras.layers.Dense(1,name='out_layer')(layer)
+    
+    model = tf.keras.models.Model(inputs=inputs,outputs=layer)
+    return model
 
-acc_list = list(itertools.chain(*sar_news))
 
-stopwords = nltk.corpus.stopwords.words('english')
-sar_list_restp = [word for word in sar_list if word.lower() not in stopwords]
-acc_list_restp = [word for word in acc_list if word.lower() not in stopwords]
 
 
 
