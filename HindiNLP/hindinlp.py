f897c31aac54a6d0c2dac2859580a3e12f4c9010
Pritindra <pritindra.d@gmail.com>
2021-08-03 14:34:52 +0530

    added hindi nlp

---
+++ HindiNLP/hindinlp.py
@@ -0,0 +1,11 @@
+# Hindi to english words translation
+# Then to detect hate speech
+
+# start with reading a senetence in hindi
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import os
+
+
